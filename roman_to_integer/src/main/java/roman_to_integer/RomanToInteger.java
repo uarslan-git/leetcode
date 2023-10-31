c6bf206dd97dfe597ec5678d133f51f9b83cc6aa
Umut Arslan <umut.arslan@klosebrothers.de>
2023-10-31 13:12:12 +0100

    Initialize Roman Numerals Problem

---
+++ roman_to_integer/src/main/java/roman_to_integer/RomanToInteger.java
@@ -0,0 +1,13 @@
+package roman_to_integer;
+
+/**
+ * Hello world!
+ *
+ */
+public class RomanToInteger
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ roman_to_integer/src/test/java/roman_to_integer/RomanToIntegerTest.java
@@ -0,0 +1,22 @@
+package roman_to_integer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RomanToIntegerTest {
+    @Test
+    public void ShouldReturn3() {
+
+    }
+
+//    @Test
+//    void ShouldReturn58() {
+//
+//    }
+//
+//    @Test
+//    void ShouldReturn1994() {
+//
+//    }
+}
